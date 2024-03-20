3c982436b9923cf4a609c995046bc0625c6f5812
leegunhui <151035633+leegunhui@users.noreply.github.com>
2024-03-20 20:07:09 +0900

    Add files via upload

---
+++ TeamProject2/TeamProject2/src/main/webapp/standard.js
@@ -0,0 +1,63 @@
+// 페이지 URL 정의
+const standardPageUrl = "file:///C:/FirstJava/TeamProject2/TeamProject2/src/main/webapp/standard.html";
+const rankPageUrl1 = "file:///C:/FirstJava/TeamProject2/TeamProject2/src/main/webapp/rank.html";
+const rankPageUrl2 = "file:///C:/FirstJava/TeamProject2/TeamProject2/src/main/webapp/rankcls.html";
+const rankPageUrl3 = "file:///C:/FirstJava/TeamProject2/TeamProject2/src/main/webapp/rankfee.html";
+const rankPageUrl4 = "file:///C:/FirstJava/TeamProject2/TeamProject2/src/main/webapp/rankmny.html";
+
+// 평가기준 페이지 연결 함수 정의
+function linkStandardPage() {
+    const standardPageLink = document.getElementById("standardPageLink");
+    standardPageLink.href = standardPageUrl;
+}
+
+// 페이지링크 페이지 연결 함수 정의
+function linkRankPage() {
+    const rankPageLink = document.getElementById("rankPageLink1");
+    rankPageLink.href = rankPageUrl1;
+}
+
+function linkRankClsPage() {
+    const rankClsPageLink = document.getElementById("rankPageLink2");
+    rankClsPageLink.href = rankPageUrl2;
+}
+
+function linkRankFeePage() {
+    const rankFeePageLink = document.getElementById("rankPageLink3");
+    rankFeePageLink.href = rankPageUrl3;
+}
+
+function linkRankMnyPage() {
+    const rankMnyPageLink = document.getElementById("rankPageLink4");
+    rankMnyPageLink.href = rankPageUrl4;
+}
+
+// 보러가기 버튼 클릭 이벤트 핸들러 추가
+document.addEventListener("DOMContentLoaded", function() {
+    const rankPageLink = document.getElementById("rankPageLink1");
+    const rankClsPageLink = document.getElementById("rankPageLink2");
+    const rankFeePageLink = document.getElementById("rankPageLink3");
+    const rankMnyPageLink = document.getElementById("rankPageLink4");
+
+    rankPageLink.addEventListener("click", function(event) {
+        event.preventDefault(); // 기본 동작 방지
+        window.location.href = rankPageUrl1;
+    });
+
+    rankClsPageLink.addEventListener("click", function(event) {
+        event.preventDefault(); // 기본 동작 방지
+        window.location.href = rankPageUrl2;
+    });
+
+    rankFeePageLink.addEventListener("click", function(event) {
+        event.preventDefault(); // 기본 동작 방지
+        window.location.href = rankPageUrl3;
+    });
+
+    rankMnyPageLink.addEventListener("click", function(event) {
+        event.preventDefault(); // 기본 동작 방지
+        window.location.href = rankPageUrl4;
+    });
+});
+// 페이지 로드 시 평가기준 페이지 컨트롤러 연결 실행
+window.onload = linkStandardPage;
